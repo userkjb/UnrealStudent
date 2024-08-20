d102461f01454adfad346b099bd56c5f44197058
kjb <kjb_jh@naver.com>
2024-08-20 18:11:43 +0900

    사용할 캐릭터 추가.

---
+++ GameBase/Source/GameBase/CharacterGameMode/PlayerCharacter.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "CharacterGameMode/CharacterDefaultCharacter.h"
+#include "PlayerCharacter.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GAMEBASE_API APlayerCharacter : public ACharacterDefaultCharacter
+{
+	GENERATED_BODY()
+	
+};
