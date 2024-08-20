d6bcdcbe7dbb9cb9934cc3e86ce723c82745ddb9
kjb <kjb_jh@naver.com>
2024-08-20 17:19:11 +0900

    Player State - HP 와 MP 계산 함수 구현.

--- GameBase/Source/GameBase/CharacterGameMode/CharacterPlayerState.cpp
+++ GameBase/Source/GameBase/CharacterGameMode/CharacterPlayerState.cpp
@@ -2,4 +2,34 @@
 
 
 #include "CharacterGameMode/CharacterPlayerState.h"
+#include "Net/UnrealNetwork.h"
+#include "Global/GameEnum.h"
 
+ACharacterPlayerState::ACharacterPlayerState()
+{
+}
+
+void ACharacterPlayerState::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+
+	DOREPLIFETIME(ACharacterPlayerState, PlayerHP);
+	DOREPLIFETIME(ACharacterPlayerState, PlayerMP);
+	DOREPLIFETIME(ACharacterPlayerState, AttackDamage);
+	DOREPLIFETIME(ACharacterPlayerState, AbilityPower);
+}
+
+void ACharacterPlayerState::InitPlayerState()
+{
+
+}
+
+void ACharacterPlayerState::HpUpdate_Implementation(EPlayerAttackType _AttackType, int _Damage)
+{
+	PlayerHP -= _Damage;
+}
+
+void ACharacterPlayerState::MpUpdate_Implementation(int _UseMPValue)
+{
+	PlayerMP -= _UseMPValue;
+}
