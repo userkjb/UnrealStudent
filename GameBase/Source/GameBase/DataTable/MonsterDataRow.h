2ceabc8c91c27c3578870ff7135a302a36433ccf
kjb <kjb_jh@naver.com>
2024-08-20 20:27:25 +0900

    대략적인 Monster의 Data 변수 선언.

--- GameBase/Source/GameBase/DataTable/MonsterDataRow.h
+++ GameBase/Source/GameBase/DataTable/MonsterDataRow.h
@@ -5,6 +5,7 @@
 #include "CoreMinimal.h"
 #include "UObject/NoExportTypes.h"
 #include "Engine/DataTable.h"
+#include "Global/GameEnum.h"
 #include "MonsterDataRow.generated.h"
 
 /**
@@ -14,5 +15,23 @@ USTRUCT(BlueprintType)
 struct FMonsterDataRow : public FTableRowBase
 {
 	GENERATED_BODY()
+
+public :
+	FORCEINLINE FString GetName() const
+	{
+		return Name;
+	}
+
+	FORCEINLINE class USkeletalMesh* GetMonsterSkeletalmesh() const
+	{
+		return MonsterSkeletalmesh;
+	}
+
+private :
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Name", meta = (AllowprivateAccess = "true"))
+	FString Name = TEXT("");
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "SkeletalMesh", meta = (AllowprivateAccess = "true"))
+	class USkeletalMesh* MonsterSkeletalmesh = nullptr;
 };
 
