a67014300f09ba85374f913584148ba6d3634147
kjb <kjb_jh@naver.com>
2024-08-20 20:14:21 +0900

    Monster 전용 Data Row struct 생성.

---
+++ GameBase/Source/GameBase/DataTable/MonsterDataRow.h
@@ -0,0 +1,18 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/NoExportTypes.h"
+#include "Engine/DataTable.h"
+#include "MonsterDataRow.generated.h"
+
+/**
+* 
+*/
+USTRUCT(BlueprintType)
+struct FMonsterDataRow : public FTableRowBase
+{
+	GENERATED_BODY()
+};
+
