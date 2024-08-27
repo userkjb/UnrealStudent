4643fea87d09b1fd5caa9b174d4721a26d3a9566
KJB <kjb_jh@naver.com>
2024-08-27 22:43:42 +0900

    GamePlay Tag 추가.

---
+++ GameBase/Source/GameBase/Global/GlobalGameplayTags.cpp
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Global/GlobalGameplayTags.h"
+
+namespace GlobalGameplayTags
+{
+	UE_DEFINE_GAMEPLAY_TAG(Player_Action_Move,   "Player.Action.Move");
+	UE_DEFINE_GAMEPLAY_TAG(Player_Action_Run,    "Player.Action.Run");
+	UE_DEFINE_GAMEPLAY_TAG(Player_Action_Jump,   "Player.Action.Jump");
+	UE_DEFINE_GAMEPLAY_TAG(Player_Action_Attack, "Player.Action.Attack");
+}
---
+++ GameBase/Source/GameBase/Global/GlobalGameplayTags.h
@@ -0,0 +1,13 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "NativeGamePlayTags.h"
+
+namespace GlobalGameplayTags
+{
+	UE_DECLARE_GAMEPLAY_TAG_EXTERN(Player_Action_Move);
+	UE_DECLARE_GAMEPLAY_TAG_EXTERN(Player_Action_Run);
+	UE_DECLARE_GAMEPLAY_TAG_EXTERN(Player_Action_Jump);
+	UE_DECLARE_GAMEPLAY_TAG_EXTERN(Player_Action_Attack);
+}
